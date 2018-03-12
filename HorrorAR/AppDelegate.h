cf9bc29b67dca13c1ac8f699ec5b0da8a7e46725
Gabe Jacobs <GabeJacobs824@gmail.com>
2018-03-12 10:44:58 -0400

    Initial Commit

---
+++ HorrorAR/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HorrorAR
+//
+//  Created by Gabe Jacobs on 3/12/18.
+//  Copyright © 2018 Gabe Jacobs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HorrorAR/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HorrorAR
+//
+//  Created by Gabe Jacobs on 3/12/18.
+//  Copyright © 2018 Gabe Jacobs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+#import <ARKit/ARKit.h>
+
+@interface ViewController : UIViewController
+
+@end
